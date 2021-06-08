68cc44d69f24b52440c51ce42f8fb12bd4307e83
Nikifor Nikolov <nikifor@nikolov.biz>
2021-06-08 12:24:27 +0300

    adopt-me react app progress v2

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,25 +1,10 @@
 import React from 'react';
 
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("h2", {}, props.animal),
-//     React.createElement("h2", {}, props.breed),
-//   ]);
-// }
-// this is calles Destructuring
 export default function Pet({ name, animal, breed }) {
-  // this is how this component is created in React
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, name),
-  //   React.createElement("h2", {}, animal),
-  //   React.createElement("h2", {}, breed),
-  // ]);
 
-  // this is how TypeScript helps for more readable code
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>{name.toUpperCase()}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
     </div>
--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Pet  from './Pet';
+import SearchParams from './searchParams';
 
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: "this-is-an-id", class: "this-is-a-class" },
-    [
-      React.createElement("h1", {}, "Adopt me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "labrador",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "cockateil",
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "stray",
-      }),
-    ]
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt me! </h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese"/>
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel"/>
+      <Pet name="Doink" animal="Cat" breed="Mixed"/>
+      <SearchParams />
+    </div>
   );
 };
 
-render(React.createElement(App), document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
